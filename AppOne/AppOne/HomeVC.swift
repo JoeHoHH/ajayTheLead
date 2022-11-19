513a6d112852fb5c848e0988a4e60ad0dc98a230
Ajaikumar Asapu <ajaikumar.asapu@sap.com>
2022-11-19 23:30:26 +0100

    HomeVC scene created and target changed to 16.0

---
+++ AppOne/AppOne/HomeVC.swift
@@ -0,0 +1,15 @@
+//
+//  HomeVC.swift
+//  AppOne
+//
+//  Created by Asapu, Ajaikumar on 19.11.22.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("HomeVC")
+    }
+}
