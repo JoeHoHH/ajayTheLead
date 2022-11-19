a2b237f1b725a2ddec6c627afd19073faf382c19
JoeHo <joerg_hofmeister@icloud.com>
2022-11-19 18:25:43 +0100

    project setup

---
+++ AppOne/AppOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppOne
+//
+//  Created by JoeHo on 19.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
