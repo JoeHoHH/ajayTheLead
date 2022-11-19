a4e107c44a0e219e09b5d3c6165c235a278758de
Ajaikumar Asapu <ajaikumar.asapu@sap.com>
2022-11-20 00:11:34 +0100

    Removed unnecessary functions and scene init updated

--- AppOne/AppOne/HomeVC.swift
+++ AppOne/AppOne/HomeVC.swift
@@ -10,7 +10,7 @@ import UIKit
 final class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .systemCyan
         print("HomeVC Created")
     }
 }
